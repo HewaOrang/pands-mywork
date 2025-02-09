4140d3b75a3c098ae35d38cc2742e2335478b2d0
Hewa Orang <hewaorang@gmail.com>
2025-02-09 13:56:10 +0000

    week02

---
+++ Week02/nameAndAge.py
@@ -0,0 +1,7 @@
+# nameAndAge.py
+# This program reads in name and Age and prints them out
+# Author: Hewa Orang
+
+name = input("Enter your name:")
+age = input("Enter your age: ")
+print('Hello ' + name +',' +  '\t' + 'your age is ' + age + '.')
