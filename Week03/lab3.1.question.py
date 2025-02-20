f55ce43b71471d05ac7ee7aa55ecebf8e3d65175
Hewa Orang <hewaorang@gmail.com>
2025-02-20 11:15:13 +0000

    week03

--- Week03/lab3.1.question.py
+++ Week03/lab3.1.question.py
@@ -3,8 +3,9 @@
 # Author: Hewa Orang
 
 # 6. The error is caused because the message tries to add int and str
-# 7. egg is a str and 100 is an int
-# 
+# 7. 100 starts with a number and vairables can't start with numbers
+# 8. int(X), float(x) & str(x) are the functions that are used to get the values of x
+
 
 message = 'I have eaten' + ' 99' + ' burritos.'
 print(message)
