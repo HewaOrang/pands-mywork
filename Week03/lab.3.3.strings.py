7d3287c3f131ff4b1b34fa208f8fdd2a1f1acb93
Hewa Orang <hewaorang@gmail.com>
2025-02-20 13:35:09 +0000

    week03

---
+++ Week03/lab.3.3.strings.py
@@ -0,0 +1,6 @@
+# lab3.3.strings.py
+# This program outputs a string in a certain format
+# Author: Hewa Orang
+
+message = 'John said\t"hi"\nI said\t\t"bye"'
+print(message)
---
+++ Week03/lab3.3.length.py
@@ -0,0 +1,7 @@
+# lab3.3.length.py
+# This program reads in a string and outputs it's length
+# Author: Hewa Orang
+
+inputString = input('Enter a string:')
+lengthOfString = len(inputString)
+print(f"The length of {inputString} is {lengthOfString} characters")
